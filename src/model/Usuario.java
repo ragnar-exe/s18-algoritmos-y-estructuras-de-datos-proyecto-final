ab22bb6e73e86254c33860317826fb0bf5b113ff
Milton <174166617+MiltonVite@users.noreply.github.com>
2024-09-08 21:02:55 -0500

    generando_constructor_y_metodos_getter_setter

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -2,5 +2,29 @@ package model;
 public class Usuario {
     private String usuario;
     private String contrasena;
+
+    public Usuario() {
+    }
+
+    public Usuario(String usuario, String contrasena) {
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
     
 }
