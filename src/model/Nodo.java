a774ddc9fa884b26f4cfebe63fb9dd66d4db1438
AngelChira <125518726+AngelChira@users.noreply.github.com>
2024-10-08 01:02:03 -0500

    Modificando la clase de Nodo

--- src/model/Nodo.java
+++ src/model/Nodo.java
@@ -1,5 +1,13 @@
 package model;
 
 public class Nodo {
+    private int info;
+    Nodo siguiente;
+
+    public Nodo(int info, Nodo siguiente) {
+        this.info = info;
+        this.siguiente = siguiente;
+    }
+    
     
 }
