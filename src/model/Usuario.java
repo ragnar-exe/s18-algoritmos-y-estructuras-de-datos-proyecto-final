b7e22a98a7462ad299e2e72e55b73f3dd67924ea
AngelChira <125518726+AngelChira@users.noreply.github.com>
2024-11-12 00:55:25 -0500

    Modificando el modelo

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -13,6 +13,11 @@ public class Usuario {
         this.idUsuario = idUsuario;
     }
 
+    public Usuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+    
+
     public Usuario(int idUsuario, String usuario) {
         this.idUsuario = idUsuario;
         this.usuario = usuario;
