da64469b109bdfe04adba864b01e2a1e2b929416
AngelChira <125518726+AngelChira@users.noreply.github.com>
2024-11-24 22:56:03 -0500

    Mejorando el agregar y actualizar para que reduzca o aumente el stock de productos

--- src/daoImpl/DetalleVentaDaoImpl.java
+++ src/daoImpl/DetalleVentaDaoImpl.java
@@ -16,7 +16,6 @@ import java.util.List;
 import javax.swing.JOptionPane;
 import model.Cliente;
 import model.Contiene;
-import model.DetalleCompra;
 import model.DetalleVenta;
 import model.Nodo;
 import model.Producto;
@@ -74,48 +73,67 @@ public class DetalleVentaDaoImpl implements IDaoGenerico<DetalleVenta> {
         return size;
     }
 
-    public int obtenerId(String texto) {
-        int id = 1;
-
-        // Verificar si el archivo existe
-        if (!Files.exists(Paths.get(FILE_IDSDVENTA))) {
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_IDSDVENTA))) {
-                writer.write("0\n");
-            } catch (IOException e) {
-                JOptionPane.showConfirmDialog(null, "Error al crear el archivo idsproductos", "ERROR", JOptionPane.ERROR_MESSAGE);
-            }
-        }
-
-        // Leer el último código del archivo
-        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_IDSDVENTA))) {
-            List<String> lines = Files.readAllLines(Paths.get(FILE_IDSDVENTA));
-            if (!lines.isEmpty()) {
-                try {
-                    int lastCode = Integer.parseInt(lines.get(lines.size() - 1).strip());
-                    id = lastCode + 1;
-                } catch (NumberFormatException e) {
-                    id = 1;
-                }
-            }
-        } catch (IOException e) {
-            JOptionPane.showConfirmDialog(null, "Error al obtener el ultimo ID de productos", "ERROR", JOptionPane.ERROR_MESSAGE);
-        }
-        return id;
-    }
-
     @Override
     public boolean actualizar(DetalleVenta obj) {
-        for (DetalleVenta dv : dVentas) {
-            if (dv != null && dv.getIdDVenta() != obj.getIdDVenta()) {
-                JOptionPane.showMessageDialog(null, "Etoy aqui");
-                return false;
-            }
-        }
+//        for (DetalleVenta dv : dVentas) {
+//            if (dv != null && dv.getIdDVenta() != obj.getIdDVenta()) {
+//                System.out.println(dv.getIdDVenta());
+//                System.out.println(obj.getIdDVenta());
+//                JOptionPane.showMessageDialog(null, "Etoy aqui");
+//                return false;
+//            }
+//        }
 
         for (int i = 0; i < dVentas.length; i++) {
             if (dVentas[i] != null && dVentas[i].getIdDVenta() == obj.getIdDVenta()) {
-                dVentas[i] = obj;
-                return true;
+                Nodo temp = IDaoContiene.inicio;
+                byte stockActualDVenta = (byte) dVentas[i].getCantidad();
+                byte stockActualizar = (byte) obj.getCantidad();
+                byte stockCalcular = (byte) (stockActualizar - stockActualDVenta);
+                if (stockCalcular > 0) {
+                    while (temp != null) {
+                        if (temp.getContiene().getIdContiene() == obj.getIdProducto()) {
+                            byte stockContiene = temp.getContiene().getStock();
+                            if (stockCalcular > stockContiene) {
+                                JOptionPane.showMessageDialog(null, "La cantidad ingresada es mayor al stock que hay. ("+temp.getContiene().getStock()+")");
+                                return false;
+                            } else {
+                                stockContiene = (byte) (temp.getContiene().getStock() - stockCalcular);
+                                if (IDaoContiene.actualizar(new Contiene(temp.getContiene().getIdContiene(),
+                                        temp.getContiene().getIdProducto(), temp.getContiene().getIdTalla(),
+                                        temp.getContiene().getIdColor(), temp.getContiene().getIdMarca(),
+                                        temp.getContiene().getPrecio(), stockContiene))) {
+                                    dVentas[i] = obj;
+                                    IDaoContiene.guardarEnArchivo();
+                                    return true;
+                                }
+                                return false;
+                            }
+                        }
+                        temp = temp.siguiente;
+                    }
+                    
+                } else if (stockCalcular < 0) {
+                    while (temp != null) {
+                        if (temp.getContiene().getIdContiene() == obj.getIdProducto()) {
+                            byte stockContiene = temp.getContiene().getStock();
+                            stockContiene = (byte) (temp.getContiene().getStock() + Math.abs(stockCalcular));
+                            if (IDaoContiene.actualizar(new Contiene(temp.getContiene().getIdContiene(),
+                                    temp.getContiene().getIdProducto(), temp.getContiene().getIdTalla(),
+                                    temp.getContiene().getIdColor(), temp.getContiene().getIdMarca(),
+                                    temp.getContiene().getPrecio(), stockContiene))) {
+                                dVentas[i] = obj;
+                                IDaoContiene.guardarEnArchivo();
+                                return true;
+                            }
+                            return false;
+                        }
+                        temp = temp.siguiente;
+                    }
+                } else {
+                    dVentas[i] = obj;
+                    return true;
+                }
             }
         }
         return false;
@@ -187,7 +205,7 @@ public class DetalleVentaDaoImpl implements IDaoGenerico<DetalleVenta> {
             }
         }
 
-        // Leer el último código del archivo
+        // Leer el ultimo codigo del archivo
         try (BufferedReader reader = new BufferedReader(new FileReader(FILE_IDSDVENTA))) {
             List<String> lines = Files.readAllLines(Paths.get(FILE_IDSDVENTA));
             if (!lines.isEmpty()) {
@@ -206,47 +224,44 @@ public class DetalleVentaDaoImpl implements IDaoGenerico<DetalleVenta> {
 
     @Override
     public boolean agregar(DetalleVenta obj) {
-//        // Verificar si hay stock suficiente
-//        if (productoInventario.getStock() >= obj.getCantidad()) {
-//            // Reducir el stock y actualizar el inventario
-//            byte stockNuevo = (byte) (productoInventario.getStock() - obj.getCantidad());
-//            System.out.println(stockNuevo);
-//            System.out.println("" + productoInventario.getIdContiene());
-////            int idContiene, int idProducto, int idTalla, int idColor, int idMarca, float precio, byte stock
-//            if (contieneDao.actualizar(new Contiene(productoInventario.getIdContiene(), productoInventario.getIdProducto(), productoInventario.getIdTalla(), productoInventario.getIdColor(), productoInventario.getIdMarca(), productoInventario.getPrecio(), stockNuevo))) {
-//                System.out.println("se agrego correcto");
-//            }else{
-//                System.out.println("NO SE PUDO AGREGAR");
-//            }
-
-        // Agregar el detalle de venta y guardar el código
-        try (BufferedWriter codigos = new BufferedWriter(new FileWriter(FILE_IDSDVENTA, true))) {
-            codigos.write(obj.getIdDVenta() + "\n");
-        } catch (IOException ex) {
-            JOptionPane.showMessageDialog(null, "Error al agregar el codigo de producto", "Error", JOptionPane.ERROR_MESSAGE);
-            return false;
-        }
-
         if (size < dVentas.length) {
-            dVentas[end] = obj;
-            end = (end + 1) % dVentas.length;
-            size++;
             Nodo temp = IDaoContiene.inicio;
             while (temp != null) {
-//                if (temp.getContiene().getIdProducto() == obj.get)
+                if (temp.getContiene().getIdContiene() == obj.getIdProducto()) {
+                    byte stockContiene = temp.getContiene().getStock();
+                    if (obj.getCantidad() > stockContiene) {
+                        JOptionPane.showMessageDialog(null, "La cantidad ingresada es mayor al stock que hay.");
+                        return false;
+                    } else {
+                        stockContiene = (byte) (temp.getContiene().getStock() - obj.getCantidad());
+                        if (IDaoContiene.actualizar(new Contiene(temp.getContiene().getIdContiene(),
+                                temp.getContiene().getIdProducto(), temp.getContiene().getIdTalla(),
+                                temp.getContiene().getIdColor(), temp.getContiene().getIdMarca(),
+                                temp.getContiene().getPrecio(),  stockContiene))) {                            
+                            try (BufferedWriter codigos = new BufferedWriter(new FileWriter(FILE_IDSDVENTA, true))) {
+                                codigos.write(obj.getIdDVenta() + "\n");
+                            } catch (IOException ex) {
+                                JOptionPane.showMessageDialog(null, "Error al agregar el codigo de detalle de venta", "Error", JOptionPane.ERROR_MESSAGE);
+                                IDaoContiene.actualizar(new Contiene(temp.getContiene().getIdContiene(),
+                                temp.getContiene().getIdProducto(), temp.getContiene().getIdTalla(),
+                                temp.getContiene().getIdColor(), temp.getContiene().getIdMarca(),
+                                temp.getContiene().getPrecio(),  temp.getContiene().getStock()));
+                                return false;
+                            }
+                            dVentas[end] = obj;
+                            end = (end + 1) % dVentas.length;
+                            size++;
+                            IDaoContiene.guardarEnArchivo();
+                            return true;
+                        } 
+                        return false;
+                    }
+                }
                 temp = temp.siguiente;
             }
-            IDaoContiene.guardarEnArchivo();
-            return true;
-        } else {
-            System.out.println("NO SE PUDO AGREGAR");
-            return false;
-        }
-//        } else {
-//            JOptionPane.showMessageDialog(null, "Stock insuficiente para el producto " + obj.getIdProducto(), "Error de stock", JOptionPane.WARNING_MESSAGE);
-//            return false;
-//        }
-
+        } 
+        JOptionPane.showMessageDialog(null,"La cola esta llena.");
+        return false;
     }
 
     public DetalleVenta[] listarDetalle() {
@@ -261,7 +276,7 @@ public class DetalleVentaDaoImpl implements IDaoGenerico<DetalleVenta> {
         IDaoObtenerLista<Producto> idaoProducto = new ProductoDaoImpl();
 
         for (DetalleVenta dv : dVentas) {
-            if (dv != null) {  // Verificación para evitar el NullPointerException
+            if (dv != null) {  // Verificaciï¿½n para evitar el NullPointerException
                 boolean coincideConId = String.valueOf(dv.getIdProducto()).contains(valorBuscar);
                 String cliente = idaoCliente.obtenerNombre(dv.getIdCliente());
                 String producto = idaoProducto.obtenerNombre(dv.getIdProducto());
